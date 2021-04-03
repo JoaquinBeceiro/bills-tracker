056f8184ca02f861c05b2b954675e8669563d145
JoaquinBeceiro <joaquinbeceiro@gmail.com>
2021-04-03 12:49:14 -0300

    Split styles

--- src/screens/Main/index.js
+++ src/screens/Main/index.js
@@ -3,7 +3,7 @@ import Record from "../Record";
 import Onboarding from "../Onboarding";
 import Data from "../Data";
 import UserContext from "../../components/userContext";
-import styled from "styled-components";
+import { ShowTable } from "./styles";
 
 const Main = (props) => {
   const userContext = useContext(UserContext);
@@ -11,20 +11,6 @@ const Main = (props) => {
   const { user } = userContext;
 
   const [showData, setShowData] = useState(false);
-
-  const ShowTable = styled.div`
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    width: 100%;
-    height: 50px;
-    background-color: #000;
-    color: #fff;
-    text-align: center;
-    font-weight: bolder;
-    padding: 5px;
-    cursor: pointer;
-  `;
 
   return (
     <div className="container">
---
+++ src/screens/Main/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const ShowTable = styled.div`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 50px;
+  background-color: #000;
+  color: #fff;
+  text-align: center;
+  font-weight: bolder;
+  padding: 5px;
+  cursor: pointer;
+`;
+
+export { ShowTable };
