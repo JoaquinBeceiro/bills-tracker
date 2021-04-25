b2527f6203c939d36a81ac555b6b8180a6aa089f
JoaquinBeceiro <joaquinbeceiro@gmail.com>
2021-04-25 20:45:34 -0300

    Splash time logic

--- src/screens/Splash/index.js
+++ src/screens/Splash/index.js
@@ -1,12 +1,14 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import UserContext from "../../config/userContext";
-import { createDoc, getTypes } from "../../services";
 import { withRouter } from "react-router";
 import { Container, Content, Title } from "./styles";
 import { checkCredentials } from "../../services";
 import Logo from "../../rsc/img/logo.svg";
 
 const Splash = (props) => {
+  const [newRoute, setNewRoute] = useState(null);
+  const [splashFinish, setSplashFinish] = useState(false);
+
   const { history } = props;
 
   const userContext = useContext(UserContext);
@@ -15,9 +17,15 @@ const Splash = (props) => {
   const checkUser = async (jsonFile, spreadsheetId) => {
     const valid = await checkCredentials(jsonFile, spreadsheetId);
     if (valid) {
-      history.push("/home");
+      setNewRoute("/home");
     }
   };
+
+  useEffect(() => {
+    setTimeout(() => {
+      setSplashFinish(true);
+    }, 1000);
+  }, []);
 
   useEffect(() => {
     if (!loading) {
@@ -25,15 +33,21 @@ const Splash = (props) => {
         const { jsonFile, spreadsheetId } = user;
         checkUser(jsonFile, spreadsheetId);
       } else {
-        history.push("/onboarding");
+        setNewRoute("/onboarding");
       }
     }
   }, [loading, user]);
 
+  useEffect(() => {
+    if (newRoute && splashFinish) {
+      history.push(newRoute);
+    }
+  }, [newRoute, splashFinish]);
+
   return (
     <Container>
       <Content>
-        <img src={Logo} />
+        <img src={Logo} alt="Logo" />
         <Title>BillsTracker</Title>
       </Content>
     </Container>
