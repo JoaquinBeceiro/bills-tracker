b00a55294827797534ef7cf6f67ad75f7f1438ed
Joaquin Beceiro <joaquinbeceiro@gmail.com>
2023-03-24 23:06:11 -0300

    Fix lottie issue

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,24 +1,16 @@
 import React from "react";
-import Lottie from "react-lottie";
+import Lottie from "react-lottie-player";
 import bouncingCoin from "../../rsc/lottie/bouncingCoin.json";
 
 import { Container } from "./styles";
-
-const defaultOptions = {
-  loop: true,
-  autoplay: true,
-  animationData: bouncingCoin,
-};
 
 const Loading = () => {
   return (
     <Container>
       <Lottie
-        options={defaultOptions}
-        height={200}
-        width={200}
-        isStopped={false}
-        isPaused={false}
+        animationData={bouncingCoin}
+        play
+        style={{ width: 200, height: 200 }}
       />
     </Container>
   );
